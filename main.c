59c70215b438b6ef5442c0dc48532e239826c083
Azzam Alotaibi <azzam.alotaiby@gmail.com>
2024-10-31 23:43:18 +0000

    extract the initializing of both menu and window for future use and to not have redundancy every time a menu is created

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 
 void cleanup_menu(MENU *menu, ITEM **items, int itemsLength);
 void destroy_win(WINDOW *localWindow);
+void setup_menu(MENU **menu, ITEM ***items, int choicesLength, const char **choices);
+void setup_window(WINDOW **windowMain, MENU **menu, int choicesLength);
 
 int main()
 {
     // The convention in C is has generally been to declare all such local variables at the top of a function [1]
+    // *APP DECISIONS 5* in the word file
     WINDOW *windowMain;
     MENU *menu;
     ITEM **items;
@@ -24,19 +27,6 @@ int main()
     // takes the size of the array and divide it by the first elements to get the full length without the NULL
     choicesLength = sizeof(choices) / sizeof(choices[0]) - 1;
 
-    // create an array pointer to an ITEM pointer and reserve the memory using calloc (contiguous allocation),
-    // while casting the return type to a pointer to an item pointer
-    // adds one cause of the NULL isn't calculated in the length
-    items = (ITEM **)calloc(choicesLength + 1, sizeof(ITEM *));
-
-    // create a new item out of every choice
-    for (int i = 0; i < choicesLength; i++)
-    {
-        items[i] = new_item(choices[i], NULL);
-    }
-    // assign NULL to the end of the array for new_menu()
-    items[choicesLength] = (ITEM *)NULL;
-
     // initializing the curses mode.
     // the cruses mode has to be initialized before calling the new_menu()
     initscr();
@@ -46,37 +36,25 @@ int main()
     noecho();
     keypad(stdscr, TRUE);
 
-    height = 20;
-    width = 50;
-    starty = (LINES - height) / 2;
-    startx = (COLS - width) / 2;
-    windowMain = newwin(height, width, starty, startx);
-    wrefresh(windowMain);
-    box(windowMain, 0, 0);
+    // for setting up the menu, items. and we only send choices wihtout adressing the pointer because C automatically sends the first element's pointer
+    setup_menu(&menu, &items, choicesLength, choices);
 
-    keypad(windowMain, TRUE);
-
-    // create the menu from the items array while casting the return type to a pointer to an item pointer
-    menu = new_menu((ITEM **)items);
-
-    // set the main window for the menu
-    set_menu_win(menu, windowMain);
-
-    // create a sub-window for items and aling it within the box using derwin() [3]
-    set_menu_sub(menu, derwin(windowMain, choicesLength, height - 1, 1, 2));
-
-    // to show the menu to the screen
-    post_menu(menu);
-
-    // refresh the stdsc
-    wrefresh(windowMain);
+    // for setting up the window.
+    setup_window(&windowMain, &menu, choicesLength);
 
     isRunning = true;
     // this is responsible for the menu navigation using menu_driver()
     // q for quitting
+    // todo extract the while to another function which accepts a function as an argument for the enter key
     while (isRunning)
     {
         input = wgetch(windowMain);
+        if (input == -1)
+        {
+            printw("error             ");
+            getch();
+            exit(0);
+        }
         // returns the position of the selected item
         const char *currentItemName;
         currentItemName = item_name(current_item(menu));
@@ -117,18 +95,21 @@ int main()
                 cleanup_menu(menu, items, choicesLength);
                 isRunning = false;
                 mvwprintw(windowMain, 5, 5, "Comeback later!");
+                break;
             }
-            // TODO menu selection
 
             break;
         case 'q':
             cleanup_menu(menu, items, choicesLength);
             isRunning = false;
+            mvwprintw(windowMain, 5, 5, "Comeback later!");
+            break;
         }
         wrefresh(windowMain);
     }
 
     wgetch(windowMain);
+
     destroy_win(windowMain);
     endwin();
     return 0;
@@ -160,6 +141,59 @@ void destroy_win(WINDOW *localWindow)
     // so we have to use wborder()
     wborder(localWindow, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
 
-    wrefresh(localWindow);
+    wrefresh(localWindow); // todo experiment with refresh after deleting the deletion of window
     delwin(localWindow);
+}
+
+void setup_menu(MENU **menu, ITEM ***items, int choicesLength, const char **choices)
+{
+    // create an array pointer to an ITEM pointer and reserve the memory using calloc (contiguous allocation),
+    // while casting the return type to a pointer to an item pointer
+    // adds one cause of the NULL isn't calculated in the length
+    *items = (ITEM **)calloc(choicesLength + 1, sizeof(ITEM *));
+
+    // create a new item out of every choice
+    for (int i = 0; i < choicesLength; i++)
+    {
+        (*items)[i] = new_item(choices[i], NULL);
+    }
+
+    // assign NULL to the end of the array for new_menu()
+    // we need *items to be between parantheses so we ensure the correct order of operations
+    // without it, it won't consider *items an array and will access the [*choicesLength]th element of a single item
+    (*items)[choicesLength] = (ITEM *)NULL;
+
+    // crate a new meun using the items list
+    *menu = new_menu(*items);
+}
+
+// *APP DECISIONS 6* in the word file
+// this function updates the variables using pointer
+void setup_window(WINDOW **windowMain, MENU **menu, int choicesLength)
+{
+
+    // height and width in characters for the window
+    int height = 20;
+    int width = 50;
+
+    // center the window
+    // LINES and COLS are global variables provided by the library for the height and width of the stdscr (default screen)
+    int starty = (LINES - height) / 2;
+    int startx = (COLS - width) / 2;
+
+    // create a new window with the assigned attributes
+    *windowMain = newwin(height, width, starty, startx);
+    // create a box around the window as a border
+    box(*windowMain, 0, 0);
+    keypad(*windowMain, TRUE);
+
+    // set the main window for the menu
+    set_menu_win(*menu, *windowMain);
+
+    // create a sub-window for items and aling it within the box using derwin() [3]
+    set_menu_sub(*menu, derwin(*windowMain, choicesLength, height - 1, 1, 2));
+
+    // to show the menu to the screen
+    post_menu(*menu);
+    wrefresh(*windowMain);
 }
